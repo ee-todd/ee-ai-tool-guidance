6cd5d67becd8aa4af27b614a2bbc522ab80a5c69
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-04-23 16:57:56 +0100

    chore: use zsh in nix-shell

--- shell.nix
+++ shell.nix
@@ -1,15 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    ruby
-    bundler
-    libxml2
-    libxslt
-    zlib
-  ];
+  buildInputs = with pkgs; [ ruby bundler libxml2 libxslt zlib ];
 
   shellHook = ''
+    /usr/bin/env zsh
     export GEM_HOME=$PWD/.gems
     export PATH=$GEM_HOME/bin:$PATH
     bundle config set path .gems
